bd0f1a3ec9c49ec6732035c2311fe03c3c2452eb
Aleksa Bubanja <aleksa3b@gmail.com>
2021-10-06 15:14:48 +0200

    styles finished, elements selected

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
 import { question1 } from "./questions/question1.js";
 import { question2 } from "./questions/question2.js";
 import { question3 } from "./questions/question3.js";
+
+const question = document.querySelector("#question");
+const optionFirst = document.querySelector(".optionFirst");
+const optionSecond = document.querySelector(".optionSecond");
+const optionThird = document.querySelector(".optionThird");
+const optionFourth = document.querySelector(".optionFourth");
+const btn = document.querySelector("#btn");
