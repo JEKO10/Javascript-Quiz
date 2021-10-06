1ac5d84c93fef4154134462a138c9300f095885c
Aleksa Bubanja <aleksa3b@gmail.com>
2021-10-06 15:02:11 +0200

    simple quiz skeleton

--- home.js
+++ home.js
@@ -1,7 +1,3 @@
 import { question1 } from "./questions/question1.js";
 import { question2 } from "./questions/question2.js";
-import { question3 } from "./questions/question3";
-
-console.log(question1);
-console.log(question2);
-console.log(question3);
+import { question3 } from "./questions/question3.js";
