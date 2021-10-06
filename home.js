f4101c869ee84eb8e620835306d783ff9acf9809
Aleksa Bubanja <aleksa3b@gmail.com>
2021-10-06 14:57:23 +0200

    file setup updated

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
-import { question1 } from "./question1.js";
-import { question2 } from "./question2.js";
-import { question3 } from "./question3.js";
+import { question1 } from "./questions/question1.js";
+import { question2 } from "./questions/question2.js";
+import { question3 } from "./questions/question3";
 
 console.log(question1);
 console.log(question2);
